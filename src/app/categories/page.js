86883e8ff0eec5aaad4f27686ba930bcc4a3b978
prakhar16 <sharmaprakhar1605@gmail.com>
2025-08-29 12:17:05 -0800

    chnaged file

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -3,8 +3,8 @@ import ProductsCategory from "@/pages/productsCategory";
 export default async function(){
 
     const response = await fetch('https://fakestoreapi.in/api/products/category')
-    const categories = await response.json();
-
+    const categories = await response.json();      
+console.log(categories);
     return(
         <ProductsCategory categories={categories}></ProductsCategory>
     )
